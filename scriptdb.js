61af0eb81dc9ca7297ef3329c2651a4c96aad800
Daniel Fabian <daniel.fabian@iubh-fernstudium.de>
2023-02-15 15:59:14 +0100

    1

--- scriptdb.js
+++ scriptdb.js
@@ -1,8 +1,24 @@
 
-            var Ziel;
-            function setzeOrt(p){
-                if (p=1) {
-                var Ziel="Afrika";}
-                document.getElementById('check').innerHTML = Ziel;
-            }
+var Ziel;
+function setzeOrt(p){
+    if (p=1) {
+    var Ziel="Afrika";}
+    document.getElementById('check').innerHTML = Ziel;
+    window.localStorage.setItem(p, Ziel);
+}
+
+function speichern() {
+    var key = document.getElementById('key').value;
+    var data = document.getElementById('data').value;
+    window.localStorage.setItem(key, data);
+   }
+   
+   function lesen() {
+    var key = document.getElementById('key').value;
+    document.getElementById('data').value = window.localStorage.getItem(key);
+   } 
     
+function ausgabe() {
+    var ausgabe = window.localStorage.getItem(1);
+    document.getElementById('check').innerHTML = ausgabe;
+}
